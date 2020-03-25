ca4828c689b16d12b9b3d56f2e5057d902abd13e
zia123123 <ziamuhamad@gmail.com>
2020-03-25 18:32:13 +0700

    Procfile for Heroku

--- resources/views/corona.blade.php
+++ resources/views/corona.blade.php
@@ -5,40 +5,42 @@
 
 <a style="margin-left: 80px; font-weight: bold; font-size: 15px;"> Last Update : {{ $last_update }}</a>
 
-<div class="container">
+<div class="container" style="padding: 20px; width: 100%; margin-top: auto;
+margin-bottom: auto;">
   <div class="row">
-    <div class="col-6 p-2 mb-2 bg-primary text-white rounded" style="margin-right: 10px">
+    <div class="col p-4 mb-4 bg-primary text-white rounded" style="margin-right: 10px; width: 40%;">
     	<div class="row">	
-      <i class="fa fa-hospital-o fa-5x" style="margin-right: 40px; font-size: 100px"></i><div style="font-size: 30px;">Total Kasus : <br> <a style="font-weight: bold; font-size: 70px;">{{ $jumlahKasus }} </a> </div></div><br>	
+      <i class="fa fa-hospital-o fa-5x" style="margin-right: 40px; font-size: 50px"></i><div style="font-size: 25px;">Total Kasus : <br> <a style="font-weight: bold; font-size: 28px;">{{ $jumlahKasus }} </a> </div></div><br>	
     </div>
-   <div class="col-sm p-2 mb-2 bg-info text-white rounded">
+   <div class="col p-4 mb-4 bg-info text-white rounded">
      <div class="row">	
-      <i class="fa fa-calendar-plus-o fa-3x" style="margin-right: 40px; font-size: 100px"></i><div style="font-size: 30px;">Kasus Hari ini : <br> <a style="font-weight: bold; font-size: 70px;">{{ $kasusHariIni }} </a> </div></div>
+      <i class="fa fa-calendar-plus-o fa-3x" style="margin-right: 40px; font-size: 50px"></i><div style="font-size: 23px;">Kasus Hari ini : <br> <a style="font-weight: bold; font-size: 28px;">{{ $kasusHariIni }} </a> </div></div>
     </div>
    
   </div>
    <div class="row">
-    <div class="col-sm p-2 mb-2 bg-danger text-white rounded" style="margin-right: 10px"; > <div class="row">	
-      <i class="fa fa-plus-square fa-5x" style="margin-right: 40px; font-size: 100px"></i><div style="font-size: 30px;">Meninggal : <br> <a style="font-weight: bold; font-size: 70px;">{{ $mati }} </a> </div></div>
+    <div class="col p-4 mb-4 bg-danger text-white rounded" style="margin-right: 10px;width: 40%;"; > <div class="row">	
+      <i class="fa fa-plus-square fa-5x" style="margin-right: 40px; font-size: 50px"></i><div style="font-size: 22px;">Meninggal : <br> <a style="font-weight: bold; font-size: 28px;">{{ $mati }} </a> </div></div>
     
     </div>
-   <div class="col-sm p-2 mb-2 bg-success text-white rounded">
+   <div class="col p-4 mb-4 bg-success text-white rounded">
      <div class="row">	
-      <i class="fa fa-heart fa-5x" style="margin-right: 40px; font-size: 100px"></i><div style="font-size: 30px;">Sembuh : <br> <a style="font-weight: bold; font-size: 70px;">{{ $sembuh }} </a> </div></div>
-    </div>
+      <i class="fa fa-heart fa-5x" style="margin-right: 40px; font-size: 50px;"></i><div style="font-size: 22px;">Sembuh : <br> <a style="font-weight: bold; font-size: 28px;">{{ $sembuh }} </a> </div></div>
+    </div> 
    
   </div>
   <div class="row">
-    <div class="col-sm p-2 mb-2 bg-warning text-white rounded" style="margin-right: 10px"; > <div class="row">	
-      <i class="fa fa-exclamation-circle e fa-5x" style="margin-right: 40px; font-size: 100px"></i><div style="font-size: 30px;">Critis : <br> <a style="font-weight: bold; font-size: 70px;">{{ $critis }} </a> </div></div>
+    <div class="col p-4 mb-4 bg-secondary text-white rounded" style="margin-right: 10px; width: 40%;"; > <div class="row">	
+      <i class="fa fa-exclamation-circle e fa-5x" style="margin-right: 40px; font-size: 50px"></i><div style="font-size: 22px;">Critis : <br> <a style="font-weight: bold;font-size: 28px;">{{ $critis }} </a> </div></div>
     
     </div>
-   <div class="col-sm p-2 mb-2 bg-dark text-white rounded">
+   <div class="col p-4 mb-4 bg-dark text-white rounded">
      <div class="row">	
-      <i class="fa fa-heartbeat fa-5x" style="margin-right: 40px; font-size: 100px"></i><div style="font-size: 30px;">Active : <br> <a style="font-weight: bold; font-size: 70px;">{{ $active }} </a> </div></div>
+      <i class="fa fa-heartbeat fa-5x" style="margin-right: 40px; font-size: 50px;"></i><div style="font-size: 22px;">Active : <br> <a style="font-weight: bold;font-size: 28px;">{{ $active }} </a> </div></div>
     </div>
-   
   </div>
+  	<center>	
+	<a style=" font-weight: bold; font-size: 15px;">Created By Zia Muhammad <br> Multimedia Research Laboratory<br> PK Hima Telkom University, Pemuda Persis Baleendah  </a>
+	</center>
 
-	
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
 
 
     <!-- Styles -->
@@ -30,7 +30,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Zia Muhammad
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
