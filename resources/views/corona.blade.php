7ebb87f9894d1b4b54aceb209864cb8a4b2ec62f
zia123123 <ziamuhamad@gmail.com>
2020-03-25 18:48:57 +0700

    Procfile for Heroku

--- resources/views/corona.blade.php
+++ resources/views/corona.blade.php
@@ -10,33 +10,33 @@ margin-bottom: auto;">
   <div class="row">
     <div class="col p-4 mb-4 bg-primary text-white rounded" style="margin-right: 10px; width: 40%;">
     	<div class="row">	
-      <i class="fa fa-hospital-o " style="margin-right: 40px; font-size: 100px;"></i><div style="font-size: 25px;">Total Kasus : <br> <a style="font-weight: bold; font-size: 28px;">{{ $jumlahKasus }} </a> </div></div><br>	
+      <i class="fa fa-hospital-o " style="margin-right: 40px; font-size: 70px;"></i><div style="font-size: 25px;">Total Kasus : <br> <a style="font-weight: bold; font-size: 28px;">{{ $jumlahKasus }} </a> </div></div><br>	
     </div>
    <div class="col p-4 mb-4 bg-info text-white rounded">
      <div class="row">	
-      <i class="fa fa-calendar-plus-o fa-3x" style="margin-right: 40px; font-size: 100px;"></i><div style="font-size: 23px;">Kasus Hari ini : <br> <a style="font-weight: bold; font-size: 28px;">{{ $kasusHariIni }} </a> </div></div>
+      <i class="fa fa-calendar-plus-o fa-3x" style="margin-right: 40px; font-size: 70px;"></i><div style="font-size: 23px;">Kasus Hari ini : <br> <a style="font-weight: bold; font-size: 28px;">{{ $kasusHariIni }} </a> </div></div>
     </div>
    
   </div>
    <div class="row">
     <div class="col p-4 mb-4 bg-danger text-white rounded" style="margin-right: 10px;width: 40%;"; > <div class="row">	
-      <i class="fa fa-plus-square " style="margin-right: 40px; font-size: 100px;"></i><div style="font-size: 22px;">Meninggal : <br> <a style="font-weight: bold; font-size: 28px;">{{ $mati }} </a> </div></div>
+      <i class="fa fa-plus-square " style="margin-right: 40px; font-size: 70px;"></i><div style="font-size: 22px;">Meninggal : <br> <a style="font-weight: bold; font-size: 28px;">{{ $mati }} </a> </div></div>
     
     </div>
    <div class="col p-4 mb-4 bg-success text-white rounded">
      <div class="row">	
-      <i class="fa fa-heart " style="margin-right: 40px; font-size: 100px;;"></i><div style="font-size: 22px;">Sembuh : <br> <a style="font-weight: bold; font-size: 28px;">{{ $sembuh }} </a> </div></div>
+      <i class="fa fa-heart " style="margin-right: 40px; font-size: 70px;;"></i><div style="font-size: 22px;">Sembuh : <br> <a style="font-weight: bold; font-size: 28px;">{{ $sembuh }} </a> </div></div>
     </div> 
    
   </div>
   <div class="row">
     <div class="col p-4 mb-4 bg-secondary text-white rounded" style="margin-right: 10px; width: 40%;"; > <div class="row">	
-      <i class="fa fa-exclamation-circle e " style="margin-right: 40px; font-size: 100px;"></i><div style="font-size: 22px;">Critis : <br> <a style="font-weight: bold;font-size: 28px;">{{ $critis }} </a> </div></div>
+      <i class="fa fa-exclamation-circle e " style="margin-right: 40px; font-size: 70px;"></i><div style="font-size: 22px;">Critis : <br> <a style="font-weight: bold;font-size: 28px;">{{ $critis }} </a> </div></div>
     
     </div>
    <div class="col p-4 mb-4 bg-dark text-white rounded">
      <div class="row">	
-      <i class="fa fa-heartbeat " style="margin-right: 40px; font-size: 100px;;"></i><div style="font-size: 22px;">Active : <br> <a style="font-weight: bold;font-size: 28px;">{{ $active }} </a> </div></div>
+      <i class="fa fa-heartbeat " style="margin-right: 40px; font-size: 70px;;"></i><div style="font-size: 22px;">Active : <br> <a style="font-weight: bold;font-size: 28px;">{{ $active }} </a> </div></div>
     </div>
   </div>
   	<center>	
